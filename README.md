232ede56225f8eeccdd9054b74f77394d435fc1f
damirz95 <turgunbaev.damir@yandex.ru>
2024-05-16 22:51:33 +0500

    add Tests

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 [![Actions Status](https://github.com/damirz95/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/damirz95/java-project-72/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/80ea87602bf89812ac20/maintainability)](https://codeclimate.com/github/damirz95/java-project-72/maintainability)
-
 [![Test Coverage](https://api.codeclimate.com/v1/badges/80ea87602bf89812ac20/test_coverage)](https://codeclimate.com/github/damirz95/java-project-72/test_coverage)
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.sql.SQLException;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +25,24 @@ public class AppTest {
             var response = client.get("/");
             assertThat(response.code()).isEqualTo(200);
             assertThat(response.body().string()).contains("Анализатор страниц");
+        });
+    }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testUrlForm() {
+        String url = "https://proselyte.net/tutorials/";
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "name=https://proselyte.net";
+            var response = client.post("/", requestBody);
+            assertThat(response.code()).isEqualTo(200);
         });
     }
 }
