a6492f4d2ca73918a93494b51b457749a013c066
damirz95 <turgunbaev.damir@yandex.ru>
2024-05-22 03:18:51 +0500

    add test7

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,18 +1,20 @@
+DROP TABLE IF EXISTS urls CASCADE;
 DROP TABLE IF EXISTS url_checks;
-DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    CONSTRAINT pk_url PRIMARY KEY (id)
 );
 
 CREATE TABLE url_checks (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls (id),
     status_code INT,
     h1 VARCHAR(255),
     title VARCHAR(255),
     description text,
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    CONSTRAINT pk_url_checks PRIMARY KEY (id)
 );
